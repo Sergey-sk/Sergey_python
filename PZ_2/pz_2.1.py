1fd9be2c1f62d5061823e48b4261317111432184
Sergey-sk <skuz13.04@gmail.com>
2024-11-30 12:03:32 +0300

    pz_2.1.py

--- PZ_2/pz_2.1.py
+++ PZ_2/pz_2.1.py
@@ -1,2 +1,5 @@
 num = int(input('Введите двузначное число:'))
-print(num // 10, num % 10, sep='\n')
+if len(str(num)) > 2 or len(str(num)) < 2:
+    print('Число не двузначное')
+else:
+    print(num // 10, num % 10, sep='\n')
