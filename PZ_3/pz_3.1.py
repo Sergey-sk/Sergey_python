c570d08ce0e56775442872b9fd425a00a8771c12
Sergey-sk <skuz13.04@gmail.com>
2024-12-07 15:10:05 +0300

    pz_3.1.py

--- PZ_3/pz_3.1.py
+++ PZ_3/pz_3.1.py
@@ -1,10 +1,26 @@
 #1. Даны три целых числа: A, B, C. Проверить истинность высказывания: «Каждое из
 # чисел A, B, C положительное».
 
-A = int(input('Введите целое число: '))
-B = int(input('Введите целое число: '))
-C = int(input('Введите целое число: '))
-if A > 0 and B > 0 and C > 0:
+A = input('Введите целое число: ')
+B = input('Введите целое число: ')
+C = input('Введите целое число: ')
+try:
+ A = int(A)
+except ValueError:
+ print("Неправильно ввели!")
+ A = input("Введите первое число: ")
+try:
+ B = int(B)
+except ValueError:
+ print("Неправильно ввели!")
+ B= input("Введите второе число: ")
+try:
+ C = int(C)
+except ValueError:
+ print("Неправильно ввели!")
+ C = input("Введите третье число: ")
+
+if int(A) > 0 and int(B) > 0 and int(C) > 0:
     print(True)
 else:
     print(False)
